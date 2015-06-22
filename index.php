4c32bd52cd997ca67b01f517aebe6013a8214b7a
Mike Chelen <michael.chelen@gmail.com>
2015-06-22 16:42:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 
 require 'vendor/autoload.php';
 
+// this package is coming from packagist
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-
 $log->addWarning('Foo');
-$log->addWarning('Bar');
 
-
+// this package is coming from git
 use HelloWorld\SayHello;
-
 echo SayHello::world();
 
 ?>
