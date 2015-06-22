c49b194afd97c700302c3367cd19ebfe7b6a3414
Michael Chelen <michael.chelen@gmail.com>
2015-06-22 13:25:35 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+
+$log->addWarning('Foo');
+$log->addWarning('Bar');
+
+?>
